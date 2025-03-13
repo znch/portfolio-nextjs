1d2c0acc0eb862fd098d35fd8e6ab8820bdde2fc
Jan Koláček <kolacek.jenik@gmail.com>
2025-03-13 13:45:49 +0100

    zmena na 2 form styly misto 3

--- src/app/component-showcase/page.tsx
+++ src/app/component-showcase/page.tsx
@@ -9,15 +9,12 @@ export default function Page() {
         <div className="w-full pt-36">
             <div className="w-full">
                 <h1 className="text-3xl text-center mb-4">Registrační formuláře</h1>
-                <div className="w-full mx-auto flex flex-col lg:flex-row justify-evenly">
+                <div className="w-full mx-auto flex flex-col md:flex-row lg:w-8/12 justify-evenly items-center">
                 <div>
                 <RegistrationFormClassic></RegistrationFormClassic>
                 </div>
                 <div>
                 <RegistrationFormModern></RegistrationFormModern>
-                </div>
-                <div>
-                <RegistrationFormSquares></RegistrationFormSquares>
                 </div>
                 </div>
                 
